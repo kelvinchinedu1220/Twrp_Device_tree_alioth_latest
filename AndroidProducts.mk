dc9e966d79c94fdb1ac3023e47651d1ccc059734
kelvinchinedu1220 <pilopilosimon1990@gmail.com>
2024-12-11 10:50:20 +0100

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -3,9 +3,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/twrp_veux.mk
+    $(LOCAL_DIR)/twrp_alioth.mk
 
 COMMON_LUNCH_CHOICES := \
-    twrp_veux-user \
-    twrp_veux-userdebug \
-    twrp_veux-eng
+    twrp_alioth-user \
+    twrp_alioth-userdebug \
+    twrp_alioth-eng
