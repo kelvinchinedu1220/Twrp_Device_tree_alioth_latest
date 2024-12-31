1c4eab4ab16e7118852f3dc592566a41018fd5ad
kelvinchinedu1220 <pilopilosimon1990@gmail.com>
2024-12-31 16:21:21 +0100

    Update and rename twrp_veux.mk to twrp_alioth.mk

--- twrp_alioth.mk
+++ twrp_alioth.mk
@@ -1,17 +1,17 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 #
-PRODUCT_RELEASE_NAME := veux
-DEVICE_PATH := device/xiaomi/veux
-# Inherit from veux device
+PRODUCT_RELEASE_NAME := alioth
+DEVICE_PATH := device/xiaomi/alioth
+# Inherit from alioth device
 $(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, vendor/twrp/config/common.mk)
-$(call inherit-product, device/xiaomi/veux/device.mk)
+$(call inherit-product, device/xiaomi/alioth/device.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := twrp_veux
-PRODUCT_DEVICE := veux
+PRODUCT_NAME := twrp_alioth
+PRODUCT_DEVICE := alioth
 PRODUCT_BRAND := POCO
 PRODUCT_MANUFACTURER := Xiaomi
-PRODUCT_MODEL := 2201116PI
+PRODUCT_MODEL := POCO F3
